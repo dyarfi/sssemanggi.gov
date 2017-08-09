c9ec5b70001f630bfed03960c0312568449d90fc
dyarfi <defrian.yarfi@gmail.com>
2017-08-09 16:18:31 +0700

    Modify

--- application/views/template/public/footer.php
+++ application/views/template/public/footer.php
@@ -127,7 +127,7 @@
 	<div id="copyrights">
 		<div class="container clearfix">
 			<div class="col_half">
-				Copyrights &copy; 2017 Simpang Susun Semanggi.<br>
+				<?php echo $this->copyright;?><br>
 				<div class="copyright-links"><a href="#">Terms of Use</a> / <a href="#">Privacy Policy</a></div>
 			</div>
 			<div class="col_half col_last tright">
